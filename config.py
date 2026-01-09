bc375db7474572ba372fed70f88ba9adda058b7b
hellolingc <hellolingc@gmail.com>
2026-01-09 18:51:33 +0800

    detect off

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 SCREEN_WIDTH = int(os.getenv("SCREEN_WIDTH", 640))
 SCREEN_HEIGHT = int(os.getenv("SCREEN_HEIGHT", 480))
 
-OPENCV_DETECT_ON = int(os.getenv("OPENCV_DETECT_ON", 1))
+OPENCV_DETECT_ON = int(os.getenv("OPENCV_DETECT_ON", 0))
 
 # 0: Don't Output 1: Output for ControlPanel, 2: Output with cs2.imshow()
 FRAME_OUTPUT_METHOD = int(os.getenv("FRAME_OUTPUT_METHOD", 1))
